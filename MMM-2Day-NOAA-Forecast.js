69747da87d168c6ddcf6f6ef2903da23d24cb7d9
Jinserk Baik <823222+jinserk@users.noreply.github.com>
2023-10-15 18:29:44 -0400

    Lint codes

--- MMM-2Day-NOAA-Forecast.js
+++ MMM-2Day-NOAA-Forecast.js
@@ -5,262 +5,265 @@
  * MIT Licensed.
  */
 
-Module.register('MMM-2Day-NOAA-Forecast', {
-
-	defaults: {
+Module.register("MMM-2Day-NOAA-Forecast", {
+  defaults: {
     lat: 0.0,
     lon: 0.0,
-		units: 'metric',
+    units: "metric",
     interval: 600000 // Every 10 mins
   },
 
-  start:  function() {
-    Log.log('Starting module: ' + this.name);
+  start: function () {
+    Log.log(`Starting module: ${this.name}`);
 
     // Set up the local values, here we construct the request url to use
     this.units = this.config.units;
     this.loaded = false;
-		this.url = 'https://api.weather.gov/points/' + this.config.lat + ',' + this.config.lon;
+    this.url = `https://api.weather.gov/points/${this.config.lat},${this.config.lon}`;
     this.forecast = [];
 
     // Trigger the first request
     this.getWeatherData(this);
   },
 
-  getStyles: function() {
-    return ['font-awesome.css', 'MMM-2Day-NOAA-Forecast.css'];
+  getStyles: function () {
+    return ["font-awesome.css", "MMM-2Day-NOAA-Forecast.css"];
   },
 
-  getDom: function() {
+  getDom: function () {
     // Set up the local wrapper
-    var wrapper = null;
+    let wrapper = null;
 
     // If we have some data to display then build the results
     if (this.loaded) {
-
-	    wrapper = document.createElement('table');
-			wrapper.className = 'forecast small';
-			wrapper.style.cssText = "border-spacing: 5px";
+      wrapper = document.createElement("table");
+      wrapper.className = "forecast small";
+      wrapper.style.cssText = "border-spacing: 5px";
 
       // title
-	    forecastRow1 = document.createElement('tr');
+      var forecastRow1 = document.createElement("tr");
 
       if (this.forecast[0].isDay) {
-	      forecastToday = document.createElement('th');
-        forecastToday.className = 'forecastTitle';
-	      forecastToday.colSpan = '2';
-	      forecastToday.innerHTML = 'Today';
+        let forecastToday = document.createElement("th");
+        forecastToday.className = "forecast-title";
+        forecastToday.colSpan = "2";
+        forecastToday.innerHTML = "Today";
 
-	      forecastTomorrow = document.createElement('th');
-        forecastTomorrow.className = 'forecastTitle';
-	      forecastTomorrow.colSpan = '2';
-	      forecastTomorrow.innerHTML = 'Tomorrow';
+        let forecastTomorrow = document.createElement("th");
+        forecastTomorrow.className = "forecast-title";
+        forecastTomorrow.colSpan = "2";
+        forecastTomorrow.innerHTML = "Tomorrow";
 
-	      forecastRow1.appendChild(forecastToday);
-	      forecastRow1.appendChild(forecastTomorrow);
-	    }
-	    else {
-	      forecastToday = document.createElement('th');
-        forecastToday.className = 'forecastTitle';
-	      forecastToday.innerHTML = 'Today';
+        forecastRow1.appendChild(forecastToday);
+        forecastRow1.appendChild(forecastTomorrow);
+      } else {
+        let forecastToday = document.createElement("th");
+        forecastToday.className = "forecast-title";
+        forecastToday.innerHTML = "Today";
 
-	      forecastTomorrow = document.createElement('th');
-        forecastTomorrow.className = 'forecastTitle';
-	      forecastTomorrow.colSpan = '2';
-	      forecastTomorrow.innerHTML = 'Tomorrow';
+        let forecastTomorrow = document.createElement("th");
+        forecastTomorrow.className = "forecast-title";
+        forecastTomorrow.colSpan = "2";
+        forecastTomorrow.innerHTML = "Tomorrow";
 
-	      forecastAfter = document.createElement('th');
-        forecastAfter.className = 'forecastTitle';
-	      forecastAfter.innerHTML = 'Day After';
+        let forecastAfter = document.createElement("th");
+        forecastAfter.className = "forecast-title";
+        forecastAfter.innerHTML = "Day After";
 
-	      forecastRow1.appendChild(forecastToday);
-	      forecastRow1.appendChild(forecastTomorrow);
-	      forecastRow1.appendChild(forecastAfter);
-	    }
+        forecastRow1.appendChild(forecastToday);
+        forecastRow1.appendChild(forecastTomorrow);
+        forecastRow1.appendChild(forecastAfter);
+      }
 
       // icons
-	    forecastRow2 = document.createElement('tr');
-	    for (var i = 0; i < 4; i++) {
-	      forecastDayNight = document.createElement('td');
-	      forecastDayNight.className = 'forecastDayNight';
+      let forecastRow2 = document.createElement("tr");
+      for (var i = 0; i < 4; i++) {
+        let forecastDayNight = document.createElement("td");
+        forecastDayNight.className = "forecast-day-night";
 
-	      forecastIcon = document.createElement('i');
-	      forecastIcon.className = 'fa fa-'
-	        + this.iconMap[this.forecast[i].icon][(this.forecast[i].isDay) ? 0 : 1]
-	        + ' fa-2x forecastIcon';
-	      forecastIcon.setAttribute('height', '50');
-	      forecastIcon.setAttribute('width', '50');
+        let forecastIcon = document.createElement("i");
+        forecastIcon.className = `fa fa-${
+          this.iconMap[this.forecast[i].icon][this.forecast[i].isDay ? 0 : 1]
+        } fa-2x forecast-icon`;
+        forecastIcon.setAttribute("height", "50");
+        forecastIcon.setAttribute("width", "50");
 
-	      forecastDayNight.appendChild(forecastIcon);
+        forecastDayNight.appendChild(forecastIcon);
 
-	      forecastRow2.appendChild(forecastDayNight);
-	    }
+        forecastRow2.appendChild(forecastDayNight);
+      }
 
       // text
-	    forecastRow3 = document.createElement('tr');
-	    for (var i = 0; i < 4; i++) {
-	      forecastText = document.createElement('td');
-	      forecastText.className = 'forecastText horizontalView bright';
-	      forecastText.innerHTML = this.forecast[i].conditions;
+      let forecastRow3 = document.createElement("tr");
+      for (let i = 0; i < 4; i++) {
+        let forecastText = document.createElement("td");
+        forecastText.className = "forecast-text horizontalView bright";
+        forecastText.innerHTML = this.forecast[i].conditions;
 
-	      forecastRow3.appendChild(forecastText);
+        forecastRow3.appendChild(forecastText);
       }
 
       // details
-	    forecastRow4 = document.createElement('tr');
-	    for (var i = 0; i < 4; i++) {
-	      forecastDetail = document.createElement('td');
-	      forecastDetail.className = 'forecastDetail';
+      let forecastRow4 = document.createElement("tr");
+      for (let i = 0; i < 4; i++) {
+        let forecastDetail = document.createElement("td");
+        forecastDetail.className = "forecast-detail";
 
-	      // Build up the details regarding temprature
-	      tempIcon = document.createElement('i');
-	      tempIcon.className = 'fa ' + ((this.forecast[i].isDay) ? 'fa-temperature-three-quarters' : 'fa-temperature-quarter') + ' fa-fw detailIcon';
-	      tempIcon.setAttribute('height', '15');
-	      tempIcon.setAttribute('width', '15');
+        // Build up the details regarding temprature
+        let tempIcon = document.createElement("i");
+        tempIcon.className = `fa ${
+          this.forecast[i].isDay
+            ? "fa-temperature-three-quarters"
+            : "fa-temperature-quarter"
+        } fa-fw detail-icon`;
+        tempIcon.setAttribute("height", "15");
+        tempIcon.setAttribute("width", "15");
 
-	      tempText = document.createElement('span');
-	      tempText.className = 'detailText';
+        let tempText = document.createElement("span");
+        tempText.className = "detail-text";
         tempText.innerHTML = this.convertTemp(this.forecast[i].temp);
-	      tempBr = document.createElement('br');
 
-	      // Build up the details regarding precipitation %
-	      rainIcon = document.createElement('i');
-	      rainIcon.className = 'fa fa-umbrella fa-fw detailIcon';
-	      rainIcon.setAttribute('height', '15');
-	      rainIcon.setAttribute('width', '15');
+        let tempBr = document.createElement("br");
 
-	      rainText = document.createElement('span');
-	      rainText.className = 'detailText';
-	      rainText.innerHTML = this.forecast[i].pop + ' %';
-	      rainBr = document.createElement('br');
+        // Build up the details regarding precipitation %
+        let rainIcon = document.createElement("i");
+        rainIcon.className = "fa fa-umbrella fa-fw detail-icon";
+        rainIcon.setAttribute("height", "15");
+        rainIcon.setAttribute("width", "15");
 
-	      // Build up the details regarding humidity %
-	      humidIcon = document.createElement('i');
-	      humidIcon.className = 'fa fa-droplet fa-fw detailIcon';
-	      humidIcon.setAttribute('height', '15');
-	      humidIcon.setAttribute('width', '15');
+        let rainText = document.createElement("span");
+        rainText.className = "detail-text";
+        rainText.innerHTML = `${this.forecast[i].pop} %`;
 
-	      humidText = document.createElement('span');
-	      humidText.className = 'detailText';
-	      humidText.innerHTML = this.forecast[i].humid + ' %';
-	      humidBr = document.createElement('br');
+        let rainBr = document.createElement("br");
 
-	      // Build up the details regarding wind
-	      windIcon = document.createElement('i');
-	      windIcon.className = 'fa fa-wind fa-fw detailIcon';
-	      windIcon.setAttribute('height', '15');
-	      windIcon.setAttribute('width', '15');
+        // Build up the details regarding humidity %
+        let humidIcon = document.createElement("i");
+        humidIcon.className = "fa fa-droplet fa-fw detail-icon";
+        humidIcon.setAttribute("height", "15");
+        humidIcon.setAttribute("width", "15");
 
-	      windText = document.createElement('span');
-	      windText.className = 'detailText';
-        windText.innerHTML = this.convertWindSpeed(this.forecast[i].wspd) + ' ' + this.forecast[i].wdir;
+        let humidText = document.createElement("span");
+        humidText.className = "detail-text";
+        humidText.innerHTML = `${this.forecast[i].humid} %`;
 
-	      // Now assemble the details
-	      forecastDetail.appendChild(tempIcon);
-	      forecastDetail.appendChild(tempText);
-	      forecastDetail.appendChild(tempBr);
-	      forecastDetail.appendChild(rainIcon);
-	      forecastDetail.appendChild(rainText);
-	      forecastDetail.appendChild(rainBr);
-	      forecastDetail.appendChild(humidIcon);
-	      forecastDetail.appendChild(humidText);
-	      forecastDetail.appendChild(humidBr);
-	      forecastDetail.appendChild(windIcon);
-	      forecastDetail.appendChild(windText);
+        let humidBr = document.createElement("br");
 
-	      forecastRow4.appendChild(forecastDetail);
-	    }
+        // Build up the details regarding wind
+        let windIcon = document.createElement("i");
+        windIcon.className = "fa fa-wind fa-fw detail-icon";
+        windIcon.setAttribute("height", "15");
+        windIcon.setAttribute("width", "15");
 
-	    wrapper.appendChild(forecastRow1);
-	    wrapper.appendChild(forecastRow2);
-	    wrapper.appendChild(forecastRow3);
-	    wrapper.appendChild(forecastRow4);
-		}
-		else {
+        let windText = document.createElement("span");
+        windText.className = "detail-text";
+        windText.innerHTML = `${this.convertWindSpeed(this.forecast[i].wspd)} ${
+          this.forecast[i].wdir
+        }`;
+
+        // Now assemble the details
+        forecastDetail.appendChild(tempIcon);
+        forecastDetail.appendChild(tempText);
+        forecastDetail.appendChild(tempBr);
+        forecastDetail.appendChild(rainIcon);
+        forecastDetail.appendChild(rainText);
+        forecastDetail.appendChild(rainBr);
+        forecastDetail.appendChild(humidIcon);
+        forecastDetail.appendChild(humidText);
+        forecastDetail.appendChild(humidBr);
+        forecastDetail.appendChild(windIcon);
+        forecastDetail.appendChild(windText);
+
+        forecastRow4.appendChild(forecastDetail);
+      }
+
+      wrapper.appendChild(forecastRow1);
+      wrapper.appendChild(forecastRow2);
+      wrapper.appendChild(forecastRow3);
+      wrapper.appendChild(forecastRow4);
+    } else {
       // Otherwise lets just use a simple div
-      wrapper = document.createElement('div');
-      wrapper.innerHTML = 'Loading ...';
+      wrapper = document.createElement("div");
+      wrapper.innerHTML = "Loading ...";
     }
 
     return wrapper;
   },
 
-  getWeatherData: function(_this) {
+  getWeatherData: function (_this) {
     // Make the initial request to the helper then set up the timer to perform the updates
-    _this.sendSocketNotification('GET-2DAY-NOAA-FORECAST', _this.url);
+    _this.sendSocketNotification("GET-2DAY-NOAA-FORECAST", _this.url);
     setTimeout(_this.getWeatherData, _this.config.interval, _this);
   },
 
-  socketNotificationReceived: function(notification, payload) {
+  socketNotificationReceived: function (notification, payload) {
     // check to see if the response was for us and used the same url
-    if (notification === 'GOT-2DAY-NOAA-FORECAST' && payload.url === this.url) {
+    if (notification === "GOT-2DAY-NOAA-FORECAST" && payload.url === this.url) {
       // we got some data so set the flag, stash the data to display then request the dom update
       this.loaded = true;
       this.forecast = payload.forecast;
-      console.log(this.forecast);
+      //console.log(this.forecast);
       this.updateDom(1000);
     }
   },
 
   iconMap: {
-    "skc": ["sun", "moon"],
-    "few": ["sun", "moon"],
-    "sct": ["cloud-sun", "cloud-moon"],
-    "bkn": ["cloud-sun", "cloud-moon"],
-    "ovc": ["cloud", "cloud"],
-    "wind_skc": ["sun", "moon"],
-    "wind_few": ["sun", "moon"],
-    "wind_sct": ["cloud-sun", "cloud-moon"],
-    "wind_bkn": ["cloud-sun", "cloud-moon"],
-    "wind_ovc": ["cloud", "cloud"],
-    "snow": ["snowflake", "snowflake"],
-    "rain_snow": ["snowflake", "snowflake"],
-    "rain_sleet": ["snowflake", "snowflake"],
-    "fzra": ["snowflake", "snowflake"],
-    "rain_fzra": ["snowflake", "snowflake"],
-    "snow_fzra": ["snowflake", "snowflake"],
-    "sleet": ["snowflake", "snowflake"],
-    "rain": ["cloud-sun-rain", "cloud-moon-rain"],
-    "rain_showers": ["cloud-rain", "cloud-rain"],
-    "rain_showers_hi": ["cloud-showers-heavy", "cloud-showers-heavy"],
-    "tsra": ["cloud-bolt", "cloud-bolt"],
-    "tsra_sct": ["bolt", "bolt"],
-    "tsra_hi": ["bolt-lightning", "bolt-lightning"],
-    "tornado": ["tornado", "tornado"],
-    "hurricane": ["hurricane", "hurricane"],
-    "tropical_storm": ["hurricane", "hurricane"],
-    "dust": ["smog", "smog"],
-    "smoke": ["smog", "smog"],
-    "haze": ["cloud-meatball", "cloud-meatball"],
-    "hot": ["temperature-arrow-up", "temperature-arrow-up"],
-    "cold": ["temperature-arrow-down", "temperature-arrow-down"],
-    "blizzard": ["wind", "wind"],
-    "fog": ["smog", "smog"],
+    skc: ["sun", "moon"],
+    few: ["sun", "moon"],
+    sct: ["cloud-sun", "cloud-moon"],
+    bkn: ["cloud-sun", "cloud-moon"],
+    ovc: ["cloud", "cloud"],
+    wind_skc: ["sun", "moon"],
+    wind_few: ["sun", "moon"],
+    wind_sct: ["cloud-sun", "cloud-moon"],
+    wind_bkn: ["cloud-sun", "cloud-moon"],
+    wind_ovc: ["cloud", "cloud"],
+    snow: ["snowflake", "snowflake"],
+    rain_snow: ["snowflake", "snowflake"],
+    rain_sleet: ["snowflake", "snowflake"],
+    fzra: ["snowflake", "snowflake"],
+    rain_fzra: ["snowflake", "snowflake"],
+    snow_fzra: ["snowflake", "snowflake"],
+    sleet: ["snowflake", "snowflake"],
+    rain: ["cloud-sun-rain", "cloud-moon-rain"],
+    rain_showers: ["cloud-rain", "cloud-rain"],
+    rain_showers_hi: ["cloud-showers-heavy", "cloud-showers-heavy"],
+    tsra: ["cloud-bolt", "cloud-bolt"],
+    tsra_sct: ["bolt", "bolt"],
+    tsra_hi: ["bolt-lightning", "bolt-lightning"],
+    tornado: ["tornado", "tornado"],
+    hurricane: ["hurricane", "hurricane"],
+    tropical_storm: ["hurricane", "hurricane"],
+    dust: ["smog", "smog"],
+    smoke: ["smog", "smog"],
+    haze: ["cloud-meatball", "cloud-meatball"],
+    hot: ["temperature-arrow-up", "temperature-arrow-up"],
+    cold: ["temperature-arrow-down", "temperature-arrow-down"],
+    blizzard: ["wind", "wind"],
+    fog: ["smog", "smog"]
   },
 
-  convertDate: function(name, isday) {
-    return name.split("T")[0].split("-").slice(1, 3).join("/") + ((isday) ? " Day" : " Night");
+  convertDate: function (name, isday) {
+    return (
+      name.split("T")[0].split("-").slice(1, 3).join("/") +
+      (isday ? " Day" : " Night")
+    );
   },
 
-  convertTemp: function(temp) {
+  convertTemp: function (temp) {
     // convert F -> C
-    return ((this.units == "metric")
-      ? (Math.round(((temp - 32) * 5) / 9) + ' &deg;C')
-      : (Math.round(temp) + ' &deg;F')
-    );
+    return this.units === "metric"
+      ? `${Math.round(((temp - 32) * 5) / 9)} &deg;C`
+      : `${Math.round(temp)} &deg;F`;
   },
 
-  convertWindSpeed: function(wspd) {
+  convertWindSpeed: function (wspd) {
     // convert mph -> m/s
-    var converted = (this.units == "metric") ? wspd.map((x) => x * 0.447) : wspd;
-    var unit = (this.units == "metric") ? "m/s" : "mph";
+    let converted = this.units === "metric" ? wspd.map((x) => x * 0.447) : wspd;
+    let unit = this.units === "metric" ? "m/s" : "mph";
 
-    return ((converted.length == 1)
-      ? (Math.round(converted[0]) + ' ' + unit)
-      : (Math.round(converted[0]) + '-'
-        + Math.round(converted[1]) + ' ' + unit)
-    );
-  },
-
-
+    return converted.length === 1
+      ? `${Math.round(converted[0])} ${unit}`
+      : `${Math.round(converted[0])}-${Math.round(converted[1])} ${unit}`;
+  }
 });
--- node_helper.js
+++ node_helper.js
@@ -5,60 +5,63 @@
  * MIT Licensed.
  */
 
-var NodeHelper = require('node_helper');
-var date = require('date-fns');
-var axios = require('axios');
-var url = require('url');
+const url = require("url");
+const NodeHelper = require("node_helper");
+const date = require("date-fns");
+const axios = require("axios");
 const Log = require("logger");
 
 module.exports = NodeHelper.create({
-
   start: function () {
-    Log.log("Starting node helper for: " + this.name);
+    Log.log(`Starting node helper for: ${this.name}`);
   },
 
-  getWeatherData: function(payload) {
-    var _this = this;
+  getWeatherData: function (payload) {
+    let _this = this;
 
-    axios.get(payload)
+    axios
+      .get(payload)
       .then(function (response) {
-        if (response.status == 200) {
+        if (response.status === 200) {
           var forecast_url = response.data.properties.forecast;
           _this.getForecastData(payload, forecast_url);
-        };
+        }
       })
       .catch(function (error) {
         Log.error(error);
-      })
+      });
   },
 
-  getForecastData: function(url1, url2) {
-    var _this = this;
-    var forecast = [];
+  getForecastData: function (url1, url2) {
+    let _this = this;
+    let forecast = [];
 
-    axios.get(url2)
+    axios
+      .get(url2)
       .then(function (response) {
         var data = response.data.properties.periods;
-        if (response.status == 200 && data.length > 4) {
+        if (response.status === 200 && data.length > 4) {
           forecast = _this.parseData(data);
-          Log.info('Got forecast data from api.weather.gov');
-        }
-        else {
-          Log.error('Got forecast data but something wrong');
+          Log.info("Got forecast data from api.weather.gov");
+        } else {
+          Log.error("Got forecast data but something wrong");
           forecast = _this.fillEmptyData();
-        };
+        }
       })
       .catch(function (error) {
         forecast = _this.fillEmptyData();
       })
-      .finally(function() {
+      .finally(function () {
         //Log.info(forecast);
-        _this.sendSocketNotification('GOT-2DAY-NOAA-FORECAST', {'url': url1, 'forecast': forecast});
+        _this.sendSocketNotification("GOT-2DAY-NOAA-FORECAST", {
+          url: url1,
+          forecast: forecast
+        });
       });
   },
 
-  parseData: function(data) {
-    var forecast = [];
+  parseData: function (data) {
+    let forecast = [];
 
     data.slice(0, 4).forEach((element, i) => {
       forecast.push({
@@ -68,64 +71,67 @@ module.exports = NodeHelper.create({
         icon: this.parseIcon(element.icon),
         conditions: element.shortForecast,
         temp: element.temperature,
-        pop: ((element.probabilityOfPrecipitation.value) ? (element.probabilityOfPrecipitation.value) : 0),
+        pop: element.probabilityOfPrecipitation.value
+          ? element.probabilityOfPrecipitation.value
+          : 0,
         humid: element.relativeHumidity.value,
-        wspd: element.windSpeed.replace("to ", "").split(" ").slice(0, -1).map(Number),
-        wdir: element.windDirection,
+        wspd: element.windSpeed
+          .replace("to ", "")
+          .split(" ")
+          .slice(0, -1)
+          .map(Number),
+        wdir: element.windDirection
       });
     });
 
     return forecast;
   },
 
-  parseIcon: function(icon_url) {
-    var data = url.parse(icon_url).pathname.split("/").slice(4);
+  parseIcon: function (icon_url) {
+    let data = url.parse(icon_url).pathname.split("/").slice(4);
 
-    if (data.length == 1) {
-      var d0 = data[0].split(",");
-      return ((d0.length == 2) ? d0[0] : data[0]);
-    }
-    else {
-      var d0 = data[0].split(",");
-      var d1 = data[1].split(",");
-      if (d0.length == 2) {
-        return ((parseInt(d0[1], 10) > 50) ? d0[0] : d1[0]);
-      }
-      else if (d1.length == 2) {
-        return ((parseInt(d1[1], 10) > 50) ? d1[0] : d0[0]);
-      }
-      else { // no pop, so choose the first
+    if (data.length === 1) {
+      let d0 = data[0].split(",");
+      return d0.length === 2 ? d0[0] : data[0];
+    } else {
+      let d0 = data[0].split(",");
+      let d1 = data[1].split(",");
+      if (d0.length === 2) {
+        return parseInt(d0[1], 10) > 50 ? d0[0] : d1[0];
+      } else if (d1.length === 2) {
+        return parseInt(d1[1], 10) > 50 ? d1[0] : d0[0];
+      } else {
+        // no pop, so choose the first
         return d0[0];
       }
-    };
+    }
   },
 
-  fillEmptyData: function() {
-    var forecast = [];
+  fillEmptyData: function () {
+    let forecast = [];
 
-    for (var i = 0; i < 4; i++) {
+    for (let i = 0; i < 4; i++) {
       forecast.push({
-        name: '--',
-        date: '--',
-        isDay: '--',
-        icon: ['not_available'],
-        conditions: 'No weather data',
-        temp: '--',
-        pop: '--',
-        humid: '--',
-        wspd: ['--'],
-        wdir: '--'
+        name: "--",
+        date: "--",
+        isDay: "--",
+        icon: ["not_available"],
+        conditions: "No weather data",
+        temp: "--",
+        pop: "--",
+        humid: "--",
+        wspd: ["--"],
+        wdir: "--"
       });
     }
 
     return forecast;
   },
 
-  socketNotificationReceived: function(notification, payload) {
+  socketNotificationReceived: function (notification, payload) {
     // Check this is for us and if it is let's get the weather data
-    if (notification === 'GET-2DAY-NOAA-FORECAST') {
+    if (notification === "GET-2DAY-NOAA-FORECAST") {
       this.getWeatherData(payload);
     }
-  },
-
+  }
 });
